ad4d7ded2f505650712b0e48a9424769a47ecbe8
Michael Paradis <paradis2@ualberta.ca>
2020-10-17 14:24:02 -0600

    Initail Websockets test. Created an app that echos back the message. (#12)

---
+++ server/roommate_reminder/roommate_reminder/routing.py
@@ -0,0 +1,11 @@
+import sockets.routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            sockets.routing.websocket_urlpatterns
+        )
+    )
+})
